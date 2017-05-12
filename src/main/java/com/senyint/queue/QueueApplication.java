2b9cdc9615b02c52a04f3a4f63fc63bb83c00bc3
hpym365 <hpym365@gmail.com>
2017-05-12 17:00:10 +0800

    readme

--- src/main/java/com/senyint/queue/QueueApplication.java
+++ src/main/java/com/senyint/queue/QueueApplication.java
@@ -2,7 +2,9 @@ package com.senyint.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
+//@SpringCloudApplication
 @SpringBootApplication
 public class QueueApplication {
 
